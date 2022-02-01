5aad42c52207817efe9d6ace5f607c594962398c
sahab <sahab.beidokhti@gmail.com>
2022-02-01 22:54:52 +0330

    add copyImage native swift code

--- ios/Classes/SwiftCopyImageToClipboardPlugin.swift
+++ ios/Classes/SwiftCopyImageToClipboardPlugin.swift
@@ -8,7 +8,37 @@ public class SwiftCopyImageToClipboardPlugin: NSObject, FlutterPlugin {
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
 
+
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+//      print("inside")
+      switch call.method{
+      case "copyImage":
+          print("inside method")
+          //inside copyImage method
+          ////let args = call.arguments as? UInt8
+          //print("saved args")
+          //get flutter data as FlutterStandardTypedData from call
+          let myFlutterData: FlutterStandardTypedData = call.arguments as! FlutterStandardTypedData
+          //make data object from myFlutterData
+          let myData = Data(myFlutterData.data)
+          //make uint8 list from myData
+          let myUInt8bytes: [UInt8] = [UInt8](myData)
+          print(myUInt8bytes)
+          //print(call.arguments)
+
+          ////let data = NSData(bytes: [args], length: 2)
+          //print("let data")
+          // let data = Data(args)
+          //make uiimage from myData
+          let image = UIImage(data: myData)
+          print("data as Data")
+          //copy image to clipboard
+          UIPasteboard.general.image = image
+          print("data copied")
+          
+          
+      default : result(FlutterMethodNotImplemented)
+      }
     result("iOS " + UIDevice.current.systemVersion)
   }
 }
