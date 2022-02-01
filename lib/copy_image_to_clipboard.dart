a428527b226a6cf867a1cf6ee5baa23da98a64b9
sahab <sahab.beidokhti@gmail.com>
2022-02-01 22:56:37 +0330

    change return type to void

--- lib/copy_image_to_clipboard.dart
+++ lib/copy_image_to_clipboard.dart
@@ -7,12 +7,13 @@ import 'package:flutter/services.dart';
 class CopyImageToClipboard {
   static const MethodChannel _channel = MethodChannel('copy_image_to_clipboard');
 
-  static Future<String?> get platformVersion async {
-    final String? version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
-  }
-  static Future copyImage(Uint8List image)async{
+  // static Future<String?> get platformVersion async {
+  //   final String? version = await _channel.invokeMethod('getPlatformVersion');
+  //   return version;
+  // }
+  static Future<void> copyImage(Uint8List image)async{
+    print('inside $image');
 
-   return await _channel.invokeListMethod("copyImage",image);
+   await _channel.invokeListMethod("copyImage",image);
   }
 }
